67bddc2a8336f934b059a295a0afff98cc259105
PLGVS <pedro.gaspar1214@gmail.com>
2025-06-27 15:39:58 -0300

    create project
    Signed-off-by: PLGVS <pedro.gaspar1214@gmail.com>

---
+++ src/main/java/com/plgvs/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.plgvs.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
