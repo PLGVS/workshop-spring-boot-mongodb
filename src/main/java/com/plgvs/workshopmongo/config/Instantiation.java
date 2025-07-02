9971fc54f42602a7ecec183e4d6192cd3ddf3b85
PLGVS <pedro.gaspar1214@gmail.com>
2025-07-02 15:18:16 -0300

    feat: adding Comments in Posts
    Signed-off-by: PLGVS <pedro.gaspar1214@gmail.com>

--- src/main/java/com/plgvs/workshopmongo/config/Instantiation.java
+++ src/main/java/com/plgvs/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.plgvs.workshopmongo.config;
 import com.plgvs.workshopmongo.domain.Post;
 import com.plgvs.workshopmongo.domain.User;
 import com.plgvs.workshopmongo.dto.AuthorDTO;
+import com.plgvs.workshopmongo.dto.CommentDTO;
 import com.plgvs.workshopmongo.repository.PostRepository;
 import com.plgvs.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,9 +41,18 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
         userRepository.save(maria);
+
+
     }
 }
