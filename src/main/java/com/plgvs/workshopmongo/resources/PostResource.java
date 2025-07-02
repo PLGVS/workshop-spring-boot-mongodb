2ee90062c796175b651d115911a4d52c1127e28e
PLGVS <pedro.gaspar1214@gmail.com>
2025-07-02 15:34:49 -0300

    feat: Find Post by Title with Query Methods
    Signed-off-by: PLGVS <pedro.gaspar1214@gmail.com>

--- src/main/java/com/plgvs/workshopmongo/resources/PostResource.java
+++ src/main/java/com/plgvs/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.plgvs.workshopmongo.resources;
 
 import com.plgvs.workshopmongo.domain.Post;
+import com.plgvs.workshopmongo.resources.util.URL;
 import com.plgvs.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -17,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
